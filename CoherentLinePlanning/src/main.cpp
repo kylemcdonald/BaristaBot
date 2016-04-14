0e0721b8928b0433ebe9287f3603dce90e651df3
Kyle McDonald <kyle@kylemcdonald.net>
2016-04-14 02:32:03 -0400

    no glut

--- CoherentLinePlanning/src/main.cpp
+++ CoherentLinePlanning/src/main.cpp
@@ -1,9 +1,6 @@
 #include "ofApp.h"
-#include "ofAppGlutWindow.h"
 
 int main() {
-	ofAppGlutWindow window;
-	window.setGlutDisplayString("rgba double samples>=8 depth");
-	ofSetupOpenGL(&window, 800, 800, OF_FULLSCREEN);
+	ofSetupOpenGL(800, 800, OF_FULLSCREEN);
 	ofRunApp(new ofApp());
 }
